55fb22bedefbad6d1c94d381e3a58699048ad3ea
jongkeun-kim <kjkandrea@gmail.com>
2021-12-18 15:15:43 +0900

    Add binary search goto default

--- tutorial/binarySearch.js
+++ tutorial/binarySearch.js
@@ -1,32 +1,20 @@
 import range from '../helper/range.js'
 
-const list = range(10000)
+const list = range(5)
 
 export default function binarySearch(list, findValue) {
-  let index = -1;
-  let howMany = list.length;
+  let index = Math.floor((list.length - 1) / 2);
+  let howMany = 1;
 
-  let middleIndex = Math.floor((list.length - 1) / 2);
-  for (let i = 0; i < list.length; i++) {
-    if (1 >= middleIndex) {
-      howMany = i;
-      break;
+  const value = list[index]
+  if (findValue !== value) return;
+    return {
+      success: index > 0,
+      index,
+      howMany
     }
-    if (list[middleIndex] !== findValue) {
-      middleIndex = Math.floor(middleIndex / 2);
-      continue;
-    }
-    index = middleIndex;
-    howMany = i + 1;
-  }
-
-  return {
-    success: index > 0,
-    index,
-    howMany
-  }
 }
 
 console.log(
-  binarySearch(list, 1000)
+  binarySearch(list, 2)
 )
