4a16c4b363a0b68b1505cf9857fe38164fdb7e2f
jongkeun-kim <kjkandrea@gmail.com>
2021-12-18 14:21:24 +0900

    Add linearSearch success happy test : green

--- tutorial/linearSearch.test.js
+++ tutorial/linearSearch.test.js
@@ -23,7 +23,19 @@ describe('linearSearch', () => {
       expect(howMany).toBe(10000)
     })
 
-    describe('10000을 찾을때에', () => {
+    describe('[성공 테스트] 1을 찾을때에', () => {
+      const { index, success } = linearSearch(list, 1)
+
+      it('index 는 1이다.', () => {
+        expect(index).toBe(1)
+      })
+
+      it('success 는 true 이다.', () => {
+        expect(success).toBe(true)
+      })
+    })
+
+    describe('[실패 테스트] 10000을 찾을때에', () => {
       const { howMany, success } = linearSearch(list, 10000)
       it('10000번의 탐색을 거친다.', () => {
         expect(howMany).toBe(10000)
