f6290c66ba987203f10980e8763a20342aa41f21
jongkeun-kim <kjkandrea@gmail.com>
2021-12-18 14:00:52 +0900

    Add range test

--- helper/range.test.js
+++ helper/range.test.js
@@ -6,5 +6,20 @@ describe('range', () => {
     it('length is 10', () => {
       expect(result.length).toBe(10)
     })
+
+    it('first index is 0', () => {
+      const firstIndex = 0;
+      expect(result[firstIndex]).toBe(0)
+    })
+
+    it('last index is 9', () => {
+      const lastIndex = result.length - 1;
+      expect(result[lastIndex]).toBe(9)
+    })
+
+    const middleIndex = Math.floor((result.length - 1) / 2)
+    it(`middleIndex(${middleIndex}) is 4`, () => {
+      expect(result[middleIndex]).toBe(4)
+    })
   })
 })
