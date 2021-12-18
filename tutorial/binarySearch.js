e49fc88fa10eff3da9e7d5500f9968a92266d145
jongkeun-kim <kjkandrea@gmail.com>
2021-12-18 14:37:48 +0900

    Add binary search happy test : green

--- tutorial/binarySearch.js
+++ tutorial/binarySearch.js
@@ -1,3 +1,21 @@
 export default function binarySearch(list, findValue) {
+  let index = -1;
+  let howMany = list.length;
 
+  let middleIndex = Math.floor((list.length - 1) / 2);
+  for (let i = 0; i < list.length; i++) {
+    if (index > 0) break;
+    if (list[middleIndex] !== findValue) {
+      middleIndex = Math.floor(middleIndex / 2);
+      continue;
+    }
+    index = middleIndex;
+    howMany = i + 1;
+  }
+
+  return {
+    success: index > 0,
+    index,
+    howMany
+  }
 }
