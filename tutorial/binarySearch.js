795d16cb837d5b3fe6f80f0b1d41ab65cb79e25b
jongkeun-kim <kjkandrea@gmail.com>
2021-12-18 15:25:59 +0900

    Add binary search if left

--- tutorial/binarySearch.js
+++ tutorial/binarySearch.js
@@ -2,19 +2,26 @@ import range from '../helper/range.js'
 
 const list = range(5)
 
-export default function binarySearch(list, findValue) {
-  let index = Math.floor((list.length - 1) / 2);
-  let howMany = 1;
+export default function binarySearch (list, findValue) {
+  let index = Math.floor((list.length - 1) / 2)
+  let value = list[index]
 
-  const value = list[index]
-  if (findValue !== value) return;
-    return {
-      success: index > 0,
+  for (let i = 0; i < list.length; i++ ) {
+    if (value === findValue) return {
+      success: true,
       index,
-      howMany
+      howMany: i + 1,
+    };
+    const isLeft = value > findValue;
+    console.log('isLeft : %s', isLeft)
+    if (isLeft) {
+      index = index / 2
+      value = list[index]
     }
+  }
+  return null;
 }
 
 console.log(
-  binarySearch(list, 2)
+  binarySearch(list, 1),
 )
