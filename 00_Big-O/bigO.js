0cb2eb027c2bd4cf531e934d7437fa35c2c6bfe6
jongkeun-kim <kjkandrea@gmail.com>
2021-12-20 19:36:01 +0900

    Add o of n square test : red

--- 00_Big-O/bigO.js
+++ 00_Big-O/bigO.js
@@ -1,8 +1,22 @@
+// O(1)
 export function Oof1 (list, fn) {
   fn(list[0])
 }
 
+// O(n)
 export function OofN (list, fn) {
+  for (let i = 0; i < list.length ;i++)
+    fn(list[i])
+}
+
+// O(n^2)
+export function OofNSquare (list, fn) {
+  for (let i = 0; i < list.length ;i++)
+    fn(list[i])
+}
+
+// O(n^2)
+export function OofNCubic (list, fn) {
   for (let i = 0; i < list.length ;i++)
     fn(list[i])
 }
--- 00_Big-O/bigO.test.js
+++ 00_Big-O/bigO.test.js
@@ -1,8 +1,8 @@
-import { Oof1, OofN } from './bigO'
+import { Oof1, OofN, OofNSquare } from './bigO'
 import range from '../helper/range'
 
 describe('bigO', () => {
-  const LENGTH = 100;
+  const LENGTH = 10;
   const sortedArray = range(LENGTH)
 
   describe('Oof1 은', () => {
@@ -20,6 +20,15 @@ describe('bigO', () => {
     it(`sortedArray length 가 ${LENGTH} 일때에 ${LENGTH}번 실행된다.`, () => {
       OofN(sortedArray, fn)
       expect(fn.mock.calls.length).toBe(LENGTH)
+    })
+  })
+
+  describe('OofNSquare 은', () => {
+    const fn = jest.fn(() => {})
+
+    it(`sortedArray length 가 ${LENGTH} 일때에 ${LENGTH * LENGTH}번 실행된다.`, () => {
+      OofNSquare(sortedArray, fn)
+      expect(fn.mock.calls.length).toBe(LENGTH * LENGTH)
     })
   })
 })
