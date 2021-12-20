f53f4d32b0f889004c8145c1f1a826115671e5b6
jongkeun-kim <kjkandrea@gmail.com>
2021-12-20 19:38:00 +0900

    Add o of n cubic test : green

--- 00_Big-O/bigO.js
+++ 00_Big-O/bigO.js
@@ -19,5 +19,7 @@ export function OofNSquare (list, fn) {
 // O(n^2)
 export function OofNCubic (list, fn) {
   for (let i = 0; i < list.length ;i++)
-    fn(list[i])
+    for (let j = 0; j < list.length ;j++)
+      for (let k = 0; k < list.length ;k++)
+        fn(list[i])
 }
