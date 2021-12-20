e7bc61c09991df265c9405e5eead273ace2e9dbc
jongkeun-kim <kjkandrea@gmail.com>
2021-12-20 19:25:11 +0900

    Add o of n test : green

--- 00_Big-O/bigO.test.js
+++ 00_Big-O/bigO.test.js
@@ -1,15 +1,25 @@
-import { Oof1 } from './bigO'
+import { Oof1, OofN } from './bigO'
 import range from '../helper/range'
 
 describe('bigO', () => {
   const LENGTH = 100;
   const sortedArray = range(LENGTH)
-  const fn = jest.fn(() => {})
 
   describe('Oof1 은', () => {
+    const fn = jest.fn(() => {})
+
     it(`sortedArray length 가 ${LENGTH} 일때에 1번 실행된다.`, () => {
       Oof1(sortedArray, fn)
       expect(fn.mock.calls.length).toBe(1)
+    })
+  })
+
+  describe('OofN 은', () => {
+    const fn = jest.fn(() => {})
+
+    it(`sortedArray length 가 ${LENGTH} 일때에 ${LENGTH}번 실행된다.`, () => {
+      OofN(sortedArray, fn)
+      expect(fn.mock.calls.length).toBe(LENGTH)
     })
   })
 })
