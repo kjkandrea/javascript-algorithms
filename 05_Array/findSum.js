88bb8dda658504c631b305e9befffb2bd25b6484
jongkeun-kim <kjkandrea@gmail.com>
2021-12-21 18:47:09 +0900

    Add findSum question my answer : green

--- 05_Array/findSum.js
+++ 05_Array/findSum.js
@@ -1,3 +1,19 @@
-export function findSum(arr, weight) {
+export const arr = [1, 2, 3, 4, 5]
+export const weight = 9
+export const correct = [3, 4]
 
+// O(n^2)
+export function findSum (arr, weight) {
+  const entry = [null, null]
+
+  for (let i = 0; i < arr.length; i++) {
+    entry[0] = arr[i]
+    for (let j = 0; j < arr.length; j++) {
+      entry[1] = arr[j]
+      if (entry[0] + entry[1] === weight) {
+        return [i, j]
+      }
+    }
+  }
+  return -1
 }
--- 05_Array/findSum.test.js
+++ 05_Array/findSum.test.js
@@ -1,9 +1,6 @@
-import { findSum } from './findSum'
+import { arr, weight, correct, findSum } from './findSum'
 
 const subject = '배열 arr 이 있고 어떤 수 weight 가 주어졌을때 합쳐서 weight 가 되는 배열 내 항목 두개의 index 를 반환하라. 만약 합쳐서 weight 가 되는 항목 두개가 존재하지 않는 경우 -1 을 반환하라.';
-const arr = [1,2,3,4,5];
-const weight = 9;
-const correct = [3,4]
 
 describe('findSum', () => {
   describe(subject, () => {
