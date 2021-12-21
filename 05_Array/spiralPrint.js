48191a8f6c63f6b7baf856e7ea657cb0d4f13d67
jongkeun-kim <kjkandrea@gmail.com>
2021-12-21 19:59:24 +0900

    Refactor spiral print answer

--- 05_Array/spiralPrint.js
+++ 05_Array/spiralPrint.js
@@ -2,23 +2,37 @@ const square = [
   [1, 2, 3, 4, 5],
   [6, 7, 8, 9, 10],
   [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20]
+  [16, 17, 18, 19, 20],
 ]
 
 spiralPrint(square)
 
 function spiralPrint (square) {
-  const width = square[0].length
-  const height = square.length
+  let topRow = 0,
+    leftCol = 0,
+    btmRow = square.length - 1,
+    rightCol = square[0].length - 1
 
-  console.log(width)
-  console.log(height)
-
-  let count = 0;
-  for (let i = 0; i < height ;i++) {
-    for (let j = 0; j <  width; j++) {
-      count++
-      console.log('%s : %s', count, square[i][j])
+  while (topRow < btmRow && leftCol < rightCol) {
+    for (let col = 0; col <= rightCol; col++) {
+      console.log(square[topRow][col])
+    }
+    topRow++
+    for (let row = topRow; row <= btmRow; row++) {
+      console.log(square[row][rightCol])
+    }
+    rightCol--
+    if (topRow <= btmRow) {
+      for (let col = rightCol; col > 0; col--) {
+        console.log(square[btmRow][col])
+      }
+      btmRow--
+    }
+    if (leftCol <= rightCol) {
+      for (let row = btmRow; row > topRow; row--) {
+        console.log(square[row][leftCol])
+      }
+      leftCol++
     }
   }
 }
