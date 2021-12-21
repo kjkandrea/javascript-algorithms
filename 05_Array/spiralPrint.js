e0ba399cbcd9b58227f70ce80c2dd7b354d49353
jongkeun-kim <kjkandrea@gmail.com>
2021-12-21 19:41:01 +0900

    Refactor spiral print answer : count

--- 05_Array/spiralPrint.js
+++ 05_Array/spiralPrint.js
@@ -8,5 +8,17 @@ const square = [
 spiralPrint(square)
 
 function spiralPrint (square) {
-  console.log(square)
+  const width = square[0].length
+  const height = square.length
+
+  console.log(width)
+  console.log(height)
+
+  let count = 0;
+  for (let i = 0; i < height ;i++) {
+    for (let j = 0; j <  width; j++) {
+      count++
+      console.log('%s : %s', count, square[i][j])
+    }
+  }
 }
