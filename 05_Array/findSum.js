54f92a4ed4bc7893e5e5f5682b42a013f5061d61
jongkeun-kim <kjkandrea@gmail.com>
2021-12-21 18:50:20 +0900

    Refactor findSum question my answer
    공간 복잡도 제거

--- 05_Array/findSum.js
+++ 05_Array/findSum.js
@@ -4,13 +4,9 @@ export const correct = [3, 4]
 
 // O(n^2)
 export function findSum (arr, weight) {
-  const entry = [null, null]
-
   for (let i = 0; i < arr.length; i++) {
-    entry[0] = arr[i]
     for (let j = 0; j < arr.length; j++) {
-      entry[1] = arr[j]
-      if (entry[0] + entry[1] === weight) {
+      if (arr[i] + arr[j] === weight) {
         return [i, j]
       }
     }
