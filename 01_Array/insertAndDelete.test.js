41fd00caf3a5323f2eec0e15934a9fdd70b31712
jongkeun-kim <kjkandrea@gmail.com>
2021-12-25 12:17:31 +0900

    Add insert length test : red

--- 01_Array/insertAndDelete.test.js
+++ 01_Array/insertAndDelete.test.js
@@ -7,8 +7,13 @@ describe('insertAndDelete', () => {
   describe('insert(array, 3, "🐈")', () => {
     it('array 의 3번째 인덱스에 🐈 가 추가된다.', () => {
         const result = insert(array, 3, '🐈')
-        console.log(result)
         expect(result[3]).toBe('🐈')
+    })
+
+    it('result.length 는 array.length + 1 이다.', () => {
+      const { length } = insert(array, 3, '🐈')
+      console.log(length)
+      expect(length).toBe(array.length + 1)
     })
   })
 })
