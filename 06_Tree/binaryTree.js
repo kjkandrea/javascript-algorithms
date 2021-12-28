9d392b448cff48bda820bec1024c45d92008d8b5
jongkeun-kim <kjkandrea@gmail.com>
2021-12-28 18:12:54 +0900

    Add tree : leaf node

--- 06_Tree/binaryTree.js
+++ 06_Tree/binaryTree.js
@@ -13,4 +13,8 @@ class Tree {
 }
 
 const tree = new Tree(new Node(9))
+tree.root.left = new Node(3)
+tree.root.right = new Node(8)
+tree.root.left.left = new Node(2)
+tree.root.left.right = new Node(5)
 console.log(tree)
