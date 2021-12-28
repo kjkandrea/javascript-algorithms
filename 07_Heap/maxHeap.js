fba21ddefcfdecc3ceb5413f625d2d79a9b5d198
jongkeun-kim <kjkandrea@gmail.com>
2021-12-28 18:30:42 +0900

    Add heep sort loop

--- 07_Heap/maxHeap.js
+++ 07_Heap/maxHeap.js
@@ -2,8 +2,21 @@ class MaxHeap {
   constructor () {
     this.heap = [null]
   }
+
+  push(value) {
+    this.heap.push(value)
+    const currentIndex = this.heap.length - 1
+    const parentIndex = Math.floor(currentIndex / 2)
+    console.log('currentIndex : %s', currentIndex)
+    console.log('parentIndex : %s', parentIndex)
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      console.log('while')
+    }
+  }
 }
 
 const heap = new MaxHeap()
+heap.push(45)
 
 console.log(heap)
