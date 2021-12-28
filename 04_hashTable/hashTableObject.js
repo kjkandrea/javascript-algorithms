bb64809c3a06953aca945bfbf041b44c6dbb5954
jongkeun-kim <kjkandrea@gmail.com>
2021-12-28 17:25:28 +0900

    Add object hash table

--- 04_hashTable/hashTableObject.js
+++ 04_hashTable/hashTableObject.js
@@ -1,4 +1,9 @@
 import users from './fixtures/users.js'
 
-const hashTable = {}
-console.log(users)
+const userHashTable = {}
+for (const user of users) {
+  userHashTable[user.name] === undefined
+    ? userHashTable[user.name] = [user]
+    : userHashTable[user.name].push(user)
+}
+console.log(userHashTable)
