ad1e450d6fa4c0038a3c947b6133d67ecfb55d1a
jongkeun-kim <kjkandrea@gmail.com>
2021-12-29 20:03:34 +0900

    Add heap pop

--- 07_Heap/maxHeap.js
+++ 07_Heap/maxHeap.js
@@ -3,21 +3,49 @@ class MaxHeap {
     this.heap = [null]
   }
 
-  push(value) {
+  push (value) {
     this.heap.push(value)
     let currentIndex = this.heap.length - 1
     let parentIndex = Math.floor(currentIndex / 2)
 
     while (parentIndex !== 0 && this.heap[parentIndex] < value) {
-      console.log('sort')
-
       const temp = this.heap[parentIndex]
-      this.heap[parentIndex] = value;
-      this.heap[currentIndex] = temp;
+      this.heap[parentIndex] = value
+      this.heap[currentIndex] = temp
 
-      currentIndex =parentIndex;
+      currentIndex = parentIndex
       parentIndex = Math.floor(currentIndex / 2)
     }
+  }
+
+  pop () {
+    const returnValue = this.heap[1]
+    this.heap[1] = this.heap.pop()
+
+    let currentIndex = 1
+    let leftIndex = 2
+    let rightIndex = 3
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex]
+      || this.heap[currentIndex] < this.heap[rightIndex]
+      ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex]
+        this.heap[currentIndex] = this.heap[rightIndex]
+        this.heap[rightIndex] = temp
+        currentIndex = rightIndex
+      }
+      else {
+        const temp = this.heap[currentIndex]
+        this.heap[currentIndex] = this.heap[leftIndex]
+        this.heap[leftIndex] = temp
+        currentIndex = leftIndex
+      }
+      leftIndex = currentIndex * 2
+      rightIndex = currentIndex * 2 + 1
+    }
+
+    return returnValue
   }
 }
 
@@ -27,5 +55,12 @@ heap.push(36)
 heap.push(54)
 heap.push(27)
 heap.push(63)
-
 console.log(heap)
+const array = [
+  heap.pop(),
+  heap.pop(),
+  heap.pop(),
+  heap.pop(),
+  heap.pop(),
+]
+console.log(array)
