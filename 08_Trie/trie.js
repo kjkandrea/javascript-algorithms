6eb865fecd45342f13a298671c68af62382a39c3
jongkeun-kim <kjkandrea@gmail.com>
2021-12-29 21:38:02 +0900

    Add Trie has

--- 08_Trie/trie.js
+++ 08_Trie/trie.js
@@ -23,8 +23,26 @@ class Trie {
       currentNode = currentNode.children.get(char)
     }
   }
+
+  has(string) {
+    let currentNode = this. root;
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        return false;
+      }
+
+      currentNode = currentNode.children.get(char)
+    }
+
+    return true
+  }
 }
 
 const trie = new Trie()
 trie.insert('cat')
+trie.insert('can')
 console.log(trie)
+console.log(trie.has('cat'))
+console.log(trie.has('can'))
+console.log(trie.has('ca'))
+console.log(trie.has('cap'))
