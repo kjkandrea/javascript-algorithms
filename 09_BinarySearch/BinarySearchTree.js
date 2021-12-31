0b707aa65b50fd842b86350b218c5bc02d550394
jongkeun-kim <kjkandrea@gmail.com>
2021-12-31 18:56:14 +0900

    Add binary search tree leaf node find place loop

--- 09_BinarySearch/BinarySearchTree.js
+++ 09_BinarySearch/BinarySearchTree.js
@@ -11,20 +11,30 @@ class BinarySearchTree {
     this.root = null
   }
 
-  insert(value) {
+  insert (value) {
     const node = new Node(value)
     if (this.root === null) {
-      this.root = node;
-      return;
+      this.root = node
+      return
     }
 
-    let currentNode = this.root;
-    if (currentNode.value < value && currentNode.right === null) {
-      currentNode.right = node
-    }
+    let currentNode = this.root
+    while (currentNode !== null) {
+      if (currentNode.value < value) {
+        if (currentNode.right === null) {
+          currentNode.right = node
+          break
+        }
+        currentNode = currentNode.right
+      }
 
-    if (currentNode.value > value && currentNode.left === null) {
-      currentNode.left = node
+      if (currentNode.value > value) {
+        if (currentNode.left === null) {
+          currentNode.left = node
+          break
+        }
+        currentNode = currentNode.left
+      }
     }
   }
 }
@@ -34,4 +44,5 @@ tree.insert(5)
 tree.insert(4)
 tree.insert(6)
 tree.insert(7)
-console.log(tree)
+tree.insert(3)
+console.log(JSON.stringify(tree))
