e74028d1b21a31fb322dbc3f47a9cfd56219491a
jongkeun-kim <kjkandrea@gmail.com>
2022-01-18 18:25:48 +0900

    Add merge sort split

--- 10_Divide_and_Conquer/mergeSort.js
+++ 10_Divide_and_Conquer/mergeSort.js
@@ -1,10 +1,17 @@
-const list = [6,5]// [6,5,3,1,8,7,2,4]
+const list = [6,5,3,4]// [6,5,3,1,8,7,2,4]
 
 console.log(
   mergeSort(list)
 )
 
 function mergeSort(list) {
+  if (list.length > 3) {
+    return [
+      mergeSort(list.slice(0, list.length/2)),
+      mergeSort(list.slice(list.length/2, list.length))
+    ]
+  }
+
   const left = list[0]
   const right = list[1]
 
