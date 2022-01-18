28e490803ed3913bd8e19a0b129787c923194677
jongkeun-kim <kjkandrea@gmail.com>
2022-01-18 19:17:43 +0900

    Add merge sort merge

--- 10_Divide_and_Conquer/mergeSort.js
+++ 10_Divide_and_Conquer/mergeSort.js
@@ -1,4 +1,4 @@
-const list = [6,5,3,4]// [6,5,3,1,8,7,2,4]
+const list = [6,5,3,1,8,7,2,4];
 
 console.log(
   mergeSort(list)
@@ -6,18 +6,39 @@ console.log(
 
 function mergeSort(list) {
   if (list.length > 3) {
-    return [
-      mergeSort(list.slice(0, list.length/2)),
-      mergeSort(list.slice(list.length/2, list.length))
+    // [ [ [ 5, 6 ], [ 1, 3 ] ], [ [ 7, 8 ], [ 2, 4 ] ] ]
+    list = [
+       mergeSort(list.slice(0, list.length/2)),
+       mergeSort(list.slice(list.length/2))
     ]
   }
 
   const left = list[0]
   const right = list[1]
 
+  if (Array.isArray(left) && Array.isArray(right)) {
+    return merge(left, right)
+  }
+
   if (left > right) {
     return [right, left]
   } else {
     return [left, right]
   }
+}
+
+function merge(left, right) {
+  const resultArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArray.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
