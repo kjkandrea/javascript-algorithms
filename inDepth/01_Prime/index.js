cde6fa48319d6ee75a7de3aea10fa968dbbf857e
jongkeun-kim <kjkandrea@gmail.com>
2022-01-21 18:34:32 +0900

    Add isPrime O(sqrt(n))

--- inDepth/01_Prime/index.js
+++ inDepth/01_Prime/index.js
@@ -2,12 +2,20 @@ import range from '../../helper/range.js'
 
 const numbers = range(100)
 
-const primes = numbers.filter(isPrime)
+const primes = numbers.filter(isPrime2)
 console.log(primes)
 
 // O(n)
 function isPrime(num) {
   for (let i = 2; i < num ; i += 1) {
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+// O(sqrt(n))
+function isPrime2(num) {
+  for (let i = 2; i * i <= num ; i += 1) {
     if (num % i === 0) return false
   }
   return true
