c04211af033572ca98f8cd658e3b81869e7ba805
jongkeun-kim <kjkandrea@gmail.com>
2022-01-21 19:08:23 +0900

    Add merge sort

--- inDepth/01_Prime/recursion.js
+++ inDepth/01_Prime/recursion.js
@@ -15,9 +15,19 @@ const mergesort = arr => {
   if (1 >= arr.length) return arr;
 
   const mid = Math.floor(arr.length / 2)
-  return [mergesort(arr.slice(0, mid)), mergesort(arr.slice(mid))]
+  return merge(mergesort(arr.slice(0, mid)), mergesort(arr.slice(mid)))
+
+  function merge (a, b) {
+    if (a.length === 0) return b;
+    if (b.length === 0) return a;
+
+    return a[0] < b[0]
+      ? [a[0], ...merge(a.slice(1), b)]
+      : [b[0], ...merge(a, b.slice(1))]
+
+  }
 }
 
 console.log(
-  JSON.stringify(mergesort([21, 10, 12, 20, 25, 13, 15, 22]))
+  mergesort([21, 10, 12, 20, 25, 13, 15, 22])
 )
