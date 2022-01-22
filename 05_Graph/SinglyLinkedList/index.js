1bd29d6bce6a426501710ace1027a0e3f1011ece
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 13:30:55 +0900

    Add SinglyLinkedList insert prototype

--- 05_Graph/SinglyLinkedList/index.js
+++ 05_Graph/SinglyLinkedList/index.js
@@ -12,5 +12,14 @@ class SinglyLinkedList {
     this.head = null
     this.size = 0;
   }
+  insert = value => {
+    if (this.head === null) {
+      this.head = new SinglyLinkedListNode(value)
+    }
+  }
   isEmpty = () => this.size === 0
 }
+
+const singlyLinkedList = new SinglyLinkedList()
+singlyLinkedList.insert(1)
+console.log(singlyLinkedList)
