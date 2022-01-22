04d352188fc595008299c3bf6844b9c9503bb47b
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 19:13:04 +0900

    Add DirectedGraph removeVertex

--- 05_Graph/DirectedGraph/index.js
+++ 05_Graph/DirectedGraph/index.js
@@ -16,6 +16,13 @@ class DirectedGraph {
       delete this.edges[orgVertex][destVertex]
     }
   }
+
+  removeVertex = vertex => {
+    Object.keys(this.edges[vertex]).forEach(adjacentVertex => {
+      this.removeEdge(adjacentVertex, vertex)
+    })
+    delete this.edges[vertex]
+  }
 }
 
 const directedGraph = new DirectedGraph()
@@ -23,8 +30,11 @@ const directedGraph = new DirectedGraph()
 directedGraph.addVertex(1)
 directedGraph.addVertex(2)
 directedGraph.addVertex(3)
+directedGraph.addVertex(4)
 directedGraph.addEdge(1, 2, 10)
 directedGraph.addEdge(2, 3, 15)
-directedGraph.removeEdge(2, 3)
+directedGraph.addEdge(3, 4, 15)
+directedGraph.removeEdge(3, 4)
+directedGraph.removeVertex(3)
 
 console.log(directedGraph.edges)
