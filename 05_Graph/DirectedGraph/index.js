7b8f332bf5f7342809f6e45219ba9374055f28ec
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 19:05:26 +0900

    Add DirectedGraph addEdge

--- 05_Graph/DirectedGraph/index.js
+++ 05_Graph/DirectedGraph/index.js
@@ -6,6 +6,10 @@ class DirectedGraph {
   addVertex = vertex => {
     this.edges[vertex] = {}
   }
+
+  addEdge = (orgVertex, destVertex, weight = 0) => {
+    this.edges[orgVertex][destVertex] = weight
+  }
 }
 
 const directedGraph = new DirectedGraph()
@@ -13,5 +17,7 @@ const directedGraph = new DirectedGraph()
 directedGraph.addVertex(1)
 directedGraph.addVertex(2)
 directedGraph.addVertex(3)
+directedGraph.addEdge(1, 2, 10)
+directedGraph.addEdge(2, 3, 15)
 
 console.log(directedGraph.edges)
