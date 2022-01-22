e2ebf60c5eb68e31b3aca9209a9a70be9f6e22d0
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 13:43:34 +0900

    Add SinglyLinkedList print

--- 02_LinkedList/SinglyLinkedList/index.js
+++ 02_LinkedList/SinglyLinkedList/index.js
@@ -2,7 +2,7 @@
 
 class SinglyLinkedListNode {
   constructor (data) {
-    this.data = data;
+    this.data = data
     this.next = null
   }
 }
@@ -10,21 +10,49 @@ class SinglyLinkedListNode {
 class SinglyLinkedList {
   constructor () {
     this.head = null
-    this.size = 0;
+    this.size = 0
   }
+
   insert = value => {
     if (this.head === null) {
       this.head = new SinglyLinkedListNode(value)
-    } else {
-      const temp = this.head;
+    }
+    else {
+      const temp = this.head
       this.head = new SinglyLinkedListNode(value)
-      this.head.next = temp;
+      this.head.next = temp
     }
   }
+
   isEmpty = () => this.size === 0
+
+  // 이건 내맘대로 만듬
+  print = () => {
+    const reverseList = []
+    if (!this.head.data) {
+      return ''
+    }
+
+    reverseList.push(this.head.data)
+
+    let nextNode = this.head.next
+    while (true) {
+      if (nextNode !== null) {
+        reverseList.push(nextNode.data)
+        nextNode = nextNode.next
+      } else {
+        console.log('tick')
+        return reverseList.reverse().join(' => ')
+      }
+    }
+  }
 }
 
 const singlyLinkedList = new SinglyLinkedList()
 singlyLinkedList.insert(1)
 singlyLinkedList.insert(2)
-console.log(singlyLinkedList)
+singlyLinkedList.insert(3)
+
+console.log(
+  singlyLinkedList.print()
+)
