6cdb2ba5e124eff8f2896b223a6c55edf9818ee5
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 19:30:03 +0900

    Add BFSDirectedGraph data

--- 05_Graph/BFS/index.js
+++ 05_Graph/BFS/index.js
@@ -1,8 +1,37 @@
 import { DirectedGraph } from '../DirectedGraph/index.js'
 
 class BFSDirectedGraph extends DirectedGraph {
-  traversBFS = () => {
+  traversBFS = (vertex, fn) => {
     const queue = []
     const visited = []
+
+    queue.push(vertex)
+
+    while(queue.length) {
+      vertex = queue.shift()
+      if (visited[vertex]) {
+        visited[vertex] = true
+      }
+    }
   }
 }
+
+const directedGraph = new BFSDirectedGraph()
+directedGraph.addVertex('A')
+directedGraph.addVertex('B')
+directedGraph.addVertex('C')
+directedGraph.addVertex('D')
+directedGraph.addVertex('E')
+directedGraph.addVertex('F')
+directedGraph.addVertex('G')
+directedGraph.addVertex('H')
+directedGraph.addVertex('I')
+directedGraph.addEdge('A', 'B', 1)
+directedGraph.addEdge('B', 'C', 1)
+directedGraph.addEdge('B', 'E', 1)
+directedGraph.addEdge('C', 'D', 1)
+directedGraph.addEdge('D', 'G', 1)
+directedGraph.addEdge('D', 'F', 1)
+directedGraph.addEdge('G', 'H', 1)
+directedGraph.addEdge('F', 'J', 1)
+console.log(directedGraph.edges)
