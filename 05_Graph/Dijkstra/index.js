9768dad37be2da57725d04ffd8e7878fc758e189
jongkeun-kim <kjkandrea@gmail.com>
2022-01-24 23:22:13 +0900

    Add DijkstraDirectedGraph dijkstra prototype

--- 05_Graph/Dijkstra/index.js
+++ 05_Graph/Dijkstra/index.js
@@ -2,8 +2,28 @@
 
 import { DirectedGraph } from '../DirectedGraph/index.js'
 
-const graph = new DirectedGraph()
+class DijkstraDirectedGraph extends DirectedGraph {
+  dijkstra = () => {
+    this.edges && Object.keys(this.edges).forEach(vertex => {
+      console.log(vertex)
+    })
+  }
+}
+
+const graph = new DijkstraDirectedGraph()
 graph.addVertex('A')
 graph.addVertex('B')
-graph.addEdge('A', 'B', 10)
-console.log(graph.edges)
+graph.addVertex('C')
+graph.addVertex('D')
+graph.addVertex('E')
+graph.addVertex('Z')
+graph.addEdge('A', 'B', 4)
+graph.addEdge('A', 'C', 8)
+graph.addEdge('B', 'C', 1)
+graph.addEdge('B', 'D', 5)
+graph.addEdge('C', 'D', 8)
+graph.addEdge('C', 'E', 10)
+graph.addEdge('D', 'E', 2)
+graph.addEdge('D', 'Z', 2)
+graph.addEdge('E', 'Z', 2)
+console.log(graph.dijkstra())
