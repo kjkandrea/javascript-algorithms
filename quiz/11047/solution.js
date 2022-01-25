9359b579979097a8d9a303441085ae212fb95673
jongkeun-kim <kjkandrea@gmail.com>
2022-01-25 19:40:36 +0900

    Create question 11047 answer

--- quiz/11047/solution.js
+++ quiz/11047/solution.js
@@ -4,24 +4,25 @@ const line = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
 const [_, currency] = line.splice(0, 1)[0].split(' ').map(Number)
 const coins = line.map(Number)
 
-// TODO 시간 초과.. 누산을 계산하는게 문제인듯
 function solution(currency, coins) {
   coins = coins.reverse()
 
-  const pocket = [0]
+  let howMany = 0;
+  let sum = 0;
   for (const coin of coins) {
     if (currency >= coin) {
-      while(currency >= pocket.reduce((a, b) => a + b, coin)) {
-        pocket.push(coin)
+      while(currency >= (sum + coin)) {
+        sum += coin
+        howMany += 1
       }
 
-      if (pocket.reduce((a, b) => a + b) === currency) break;
+      if (sum === currency) break;
     }
   }
 
-  return pocket
+  return howMany
 }
 
 console.log(
-  solution(currency, coins).length - 1
+  solution(currency, coins)
 )
