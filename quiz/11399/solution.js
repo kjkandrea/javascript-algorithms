663a400b06dfdf570f541e1adc48837054ec28f3
jongkeun-kim <kjkandrea@gmail.com>
2022-01-26 20:22:34 +0900

    Add quiz fail answer

--- quiz/11399/solution.js
+++ quiz/11399/solution.js
@@ -3,8 +3,15 @@ import fs from 'fs';
 const line = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
 const peoples = line[1].split(' ').map(Number)
 
+// TODO: 문제 이해를 잘못한듯?
 function solution(peoples) {
-  console.log(peoples)
+  let prev = 0;
+  let total = 0;
+  for (const people of peoples) {
+    prev += people
+    total += prev
+  }
+  return total
 }
 
 console.log(
