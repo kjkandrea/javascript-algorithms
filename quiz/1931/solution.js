365a9a93351ea3fac26c58451898d657c48446ab
jongkeun-kim <kjkandrea@gmail.com>
2022-01-26 21:06:16 +0900

    Fix 1931 quiz todo

--- quiz/1931/solution.js
+++ quiz/1931/solution.js
@@ -3,10 +3,23 @@ import fs from 'fs';
 const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
 const meetingTimes = lines.map(line => {
   const [start, end] = line.split(' ')
-  return [Number(start), Number(end)]
+  return [Number(end), Number(start)]
 })
 
 function solution (meetingTimes) {
+  meetingTimes = meetingTimes.sort(([a], [b]) => a - b);
+  let total = 0;
+  let used = [0, 0]
+  for (const times of meetingTimes) {
+    if (used[0] === 0) {
+      used = times
+      total += 1
+      continue;
+    }
+
+    // 여기서 가능한 시간 추려서 total 낼 예정!
+    console.log(times)
+  }
   console.log(meetingTimes)
 }
 
