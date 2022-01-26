d7a6f5334ab3f84d8f972a4bdf8306c822c1a209
jongkeun-kim <kjkandrea@gmail.com>
2022-01-26 20:24:21 +0900

    Add quiz answer

--- quiz/11399/solution.js
+++ quiz/11399/solution.js
@@ -3,8 +3,9 @@ import fs from 'fs';
 const line = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
 const peoples = line[1].split(' ').map(Number)
 
-// TODO: 문제 이해를 잘못한듯?
 function solution(peoples) {
+  peoples = peoples.sort((a, b)=> a - b)
+
   let prev = 0;
   let total = 0;
   for (const people of peoples) {
