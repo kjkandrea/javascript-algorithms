e94d9d6af74f44565eb8a52e0bd1f3bc7a08cc64
jongkeun-kim <kjkandrea@gmail.com>
2022-01-26 20:36:27 +0900

    Fix 1931 quiz parser

--- quiz/1931/solution.js
+++ quiz/1931/solution.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 
-const line = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
-const meetingTimes = line.map(([start, end]) => [
-  Number(start),
-  Number(end),
-])
+const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
+const meetingTimes = lines.map(line => {
+  const [start, end] = line.split(' ')
+  return [Number(start), Number(end)]
+})
 
 function solution (meetingTimes) {
   console.log(meetingTimes)
