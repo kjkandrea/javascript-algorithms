f5093bccb222661ed39b6846d6f3aae313486a2a
jongkeun-kim <kjkandrea@gmail.com>
2022-01-27 21:38:03 +0900

    Add 10773 answer

---
+++ quiz/10773/solution.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const numbers = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').map(Number).splice(1);
+
+function solution(numbers) {
+  return numbers.reduce((stack, num) => {
+    num === 0 ? stack.pop() : stack.push(num)
+    return stack;
+  }, []).concat([0]).reduce((a, b) => a + b)
+}
+
+console.log(
+  solution(numbers)
+)
