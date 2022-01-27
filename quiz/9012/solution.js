9e58f2b5fd4e629cb9e73f264286d3c90565df0d
jongkeun-kim <kjkandrea@gmail.com>
2022-01-27 21:27:41 +0900

    Add 9012 answer

--- quiz/9012/solution.js
+++ quiz/9012/solution.js
@@ -1,16 +1,16 @@
 import fs from 'fs';
 
-const brackets = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
+const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
 
+// Stack
 function solution (brackets) {
   let matcher = 0;
   for (const char of brackets) {
     if (0 > matcher) return 'NO';
+    if (char === '(') matcher += 1
+    if (char === ')') matcher -= 1
   }
   return matcher === 0 ? 'YES' : 'NO'
 }
 
-
-console.log(
-  solution(brackets)
-)
+lines.forEach(brackets => console.log(solution(brackets)))
