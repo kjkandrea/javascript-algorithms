d86b1030541657b40c59a0b8fc17d1505a14216d
jongkeun-kim <kjkandrea@gmail.com>
2022-01-28 20:46:16 +0900

    Add 10799 answer : fail

--- quiz/10799/solution.js
+++ quiz/10799/solution.js
@@ -2,8 +2,23 @@ import fs from 'fs';
 
 const bars = fs.readFileSync('./input.txt', 'utf8').trim()
 
+// TODO: fail
 function solution(bars) {
-  return bars;
+  let count = 0;
+  bars.split('').reduce((prev, cur) => {
+    if (prev === cur) {
+      count += 1
+    }
+
+    if (prev === '(' && cur === ')') {
+      count += 1
+    }
+
+    prev = cur;
+    return prev;
+  }, null)
+
+  return count;
 }
 
 console.log(
