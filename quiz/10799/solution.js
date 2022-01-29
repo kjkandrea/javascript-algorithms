299904a8a6bbe614c8060cd2a8af530e805b7dc1
jongkeun-kim <kjkandrea@gmail.com>
2022-01-29 22:13:49 +0900

    Add 10799 replace raiser

--- quiz/10799/solution.js
+++ quiz/10799/solution.js
@@ -4,21 +4,11 @@ const bars = fs.readFileSync('./input.txt', 'utf8').trim()
 
 // TODO: fail
 function solution(bars) {
+  let stack = 0;
   let count = 0;
-  bars.split('').reduce((prev, cur) => {
-    if (prev === cur) {
-      count += 1
-    }
 
-    if (prev === '(' && cur === ')') {
-      count += 1
-    }
-
-    prev = cur;
-    return prev;
-  }, null)
-
-  return count;
+  const raiserBars = bars.replaceAll('()', '|')
+  return raiserBars;
 }
 
 console.log(
