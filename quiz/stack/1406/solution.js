ea4fe0f29aafa5cd17f690567c3f76e0221c2d0e
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 16:52:40 +0900

    Add 1406 my answer

--- quiz/stack/1406/solution.js
+++ quiz/stack/1406/solution.js
@@ -3,12 +3,32 @@ import fs from "fs"
 const inputs = fs.readFileSync('./input.txt', 'utf8').split('\n')
 const [initialValue] = inputs.splice(0, 2)
 
+// 시간 초과
 function solution(initialValue, inputs) {
   const stringArray = initialValue.split('')
   let cursor = stringArray.length - 1
 
-  console.log(stringArray)
-  console.log(cursor)
+  inputs.forEach(input => {
+    if (input.startsWith('P')) {
+      const [_, payload] = input.split(' ')
+      cursor += 1
+      stringArray.splice(cursor, 0, payload)
+      return;
+    }
+
+    if (input.startsWith('B')) {
+      if (stringArray[cursor]) {
+        stringArray.splice(cursor, 1)
+        if (cursor !== -1) cursor -= 1
+      }
+      return;
+    }
+
+    if (input === 'D' && stringArray.length - 1 > cursor) cursor += 1
+    if (input === 'L' && cursor > -1) cursor -= 1
+  })
+
+  console.log(stringArray.join(''))
 }
 
 solution(
