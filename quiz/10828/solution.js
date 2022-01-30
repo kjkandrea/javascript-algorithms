076e81eae0598fdac2ebfdbcf3f3669a4b306531
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 13:09:01 +0900

    Add 10828 prompt structure

--- quiz/10828/solution.js
+++ quiz/10828/solution.js
@@ -1,6 +1,9 @@
-import fs from "fs"
+import fs from 'fs'
 
-const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
+const lines = fs.readFileSync('./input.txt', 'utf8').
+  trim().
+  split('\n').
+  splice(1)
 const messages = lines.map(line => {
   const [left, right] = line.split(' ')
   return right ? [left, Number(right)] : [left, null]
@@ -8,12 +11,42 @@ const messages = lines.map(line => {
 
 class Stack {
   constructor () {
-    console.log('hello')
+    this.stack = []
+  }
+
+  push = payload => ['push', payload]
+
+  pop = () => 'pop'
+
+  get size () {
+    return 'size'
+  }
+
+  get empty () {
+    return 'empty'
+  }
+
+  get top () {
+    return 'top'
   }
 }
 
-function solution(messages) {
-  console.log(messages)
+function solution (messages) {
+  const stack = new Stack()
+
+  messages.forEach(([name, payload]) => {
+    switch (name) {
+      case 'push':
+        stack.push(payload)
+        break
+      case 'pop':
+        console.log(stack.pop())
+        break
+      default:
+        console.log(stack[name])
+        break
+    }
+  })
 }
 
 solution(messages)
