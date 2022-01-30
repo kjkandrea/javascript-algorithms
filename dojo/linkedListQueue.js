c39662eb4eb17292ac1390abc2a1c931b5d0e08f
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 21:44:03 +0900

    Create queue : enqueue

--- dojo/linkedListQueue.js
+++ dojo/linkedListQueue.js
@@ -12,14 +12,19 @@ class Queue {
     this.tail = null
   }
 
-  enqueue() {
-
+  enqueue(value) {
+    const node = new Node(value)
+    if (this.head === null) {
+      this.tail = node
+      this.head = this.tail
+    }
   }
 }
 
 function main () {
   const queue = new Queue()
   queue.enqueue(1)
+  console.log(queue)
 }
 
 main()
