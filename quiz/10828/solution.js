27327f57c4dbe1f887851f41b184cfc617b503a6
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 13:02:11 +0900

    Add 10828 parse

--- quiz/10828/solution.js
+++ quiz/10828/solution.js
@@ -1,11 +1,19 @@
 import fs from "fs"
 
 const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n').splice(1);
-
-console.log(lines)
+const messages = lines.map(line => {
+  const [left, right] = line.split(' ')
+  return right ? [left, Number(right)] : [left, null]
+})
 
 class Stack {
   constructor () {
     console.log('hello')
   }
 }
+
+function solution(messages) {
+  console.log(messages)
+}
+
+solution(messages)
