e4f50d69456275406031434120c99e96f5206c12
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 21:48:44 +0900

    Add queue : enqueue

--- dojo/linkedListQueue.js
+++ dojo/linkedListQueue.js
@@ -15,15 +15,27 @@ class Queue {
   enqueue(value) {
     const node = new Node(value)
     if (this.head === null) {
+      this.head = node
       this.tail = node
-      this.head = this.tail
+    } else {
+      this.tail.next = node
+      this.tail = node
     }
+  }
+
+  dequeue() {
+    const value = this.head.value
+    this.head = this.head.next;
+    return value;
   }
 }
 
 function main () {
   const queue = new Queue()
   queue.enqueue(1)
+  queue.enqueue(2)
+  // console.log(queue.dequeue())
+  //console.log(queue.dequeue())
   console.log(queue)
 }
 
