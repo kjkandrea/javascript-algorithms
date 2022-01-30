517b2cacc9b76ac3402c0f32aa0eadf011cd12de
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 16:54:31 +0900

    Add 1406 todo

--- quiz/stack/1406/solution.js
+++ quiz/stack/1406/solution.js
@@ -3,7 +3,12 @@ import fs from "fs"
 const inputs = fs.readFileSync('./input.txt', 'utf8').split('\n')
 const [initialValue] = inputs.splice(0, 2)
 
-// 시간 초과
+/**
+ * TODO: 시간 초과
+ *
+ * 1) 배열의 맨 끝에서만 삽입 또는 삭제 연산을 수행하도록 설계하기
+ * 2) 임의의 위치에 원소를 삽입하거나 삭제하는 연산을 O(1) 에 수행할 수 있는 자료구조 사용하기
+ **/
 function solution(initialValue, inputs) {
   const stringArray = initialValue.split('')
   let cursor = stringArray.length - 1
