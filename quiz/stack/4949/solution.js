2406b6a9e570289ea6d61421af12d033744270d4
jongkeun-kim <kjkandrea@gmail.com>
2022-01-30 14:20:30 +0900

    Move 4949 my answer

--- quiz/stack/4949/solution.js
+++ quiz/stack/4949/solution.js
@@ -2,8 +2,34 @@ import fs from 'fs';
 
 const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n')
 
-function balanceJudgment() {
+function balanceJudgment(line) {
+  const brackets = line.replace(/[^()\[\]]/g, '')
+  if (!brackets) return 'yes'
+
+  const stack = []
+  for (const bracket of brackets) {
+    if (bracket === '(' || bracket === '[') {
+      stack.push(bracket)
+    }
+    if (bracket === ')') {
+      if (stack[stack.length -1] === '(') {
+        stack.pop()
+      } else {
+        return 'no'
+      }
+    }
+
+    if (bracket === ']') {
+      if (stack[stack.length -1] === '[') {
+        stack.pop()
+      } else {
+        return 'no'
+      }
+    }
+  }
+
   return 'yes'
+
 }
 
 // Stack
