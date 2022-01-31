2d39263df5fa918f662de1ddae1cbaad8657869c
jongkeun-kim <kjkandrea@gmail.com>
2022-01-31 12:15:35 +0900

    Add 9093 string reverse

--- quiz/string/9093/solution.js
+++ quiz/string/9093/solution.js
@@ -1,9 +1,22 @@
-import fs from "fs"
+import fs from 'fs'
 
 const lines = fs.readFileSync('./input.txt', 'utf8').split('\n')
 
-function solution(lines) {
+function reverse (word) {
+  let newWord = ''
+
+  for (let i = word.length - 1; i >= 0; i -= 1) {
+    newWord += word[i]
+  }
+
+  return newWord;
+}
+
+function solution (lines) {
   console.log(lines)
 }
 
-solution(lines)
+// solution(lines)
+console.log(
+reverse('pizza')
+)
