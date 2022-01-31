1336125ac1d2491beb3cb90c2ee45f08e6fa7a88
jongkeun-kim <kjkandrea@gmail.com>
2022-01-31 14:01:54 +0900

    Add 2164

---
+++ quiz/queue/2164/solution.js
@@ -0,0 +1,26 @@
+import fs from "fs"
+
+const range = Number(fs.readFileSync('./input.txt', 'utf8'))
+
+class Node {
+  constructor () {
+
+  }
+}
+
+class Queue {
+  constructor () {
+
+  }
+}
+
+function generateQueue(range) {
+  const queue = new Queue()
+}
+
+function solution (range) {
+  const queue = generateQueue()
+  console.log(queue)
+}
+
+solution(range)
