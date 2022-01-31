ca4eb1882b55d40f6bf2bf15e452de7b997a393c
jongkeun-kim <kjkandrea@gmail.com>
2022-01-31 14:37:36 +0900

    Add 1158 quiz

---
+++ quiz/queue/1158/solution.js
@@ -0,0 +1,10 @@
+import fs from "fs"
+
+const [n, k] = fs.readFileSync('./input.txt', 'utf8').split(' ').map(Number)
+
+function solution (n, k) {
+  const result = []
+  console.log(n, k)
+}
+
+solution(n, k)
