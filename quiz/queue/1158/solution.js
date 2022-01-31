4b13797a0686ba370ff8caf7890384f8526d6ab5
jongkeun-kim <kjkandrea@gmail.com>
2022-01-31 14:43:01 +0900

    Add 1158 quiz

--- quiz/queue/1158/solution.js
+++ quiz/queue/1158/solution.js
@@ -4,7 +4,17 @@ const [n, k] = fs.readFileSync('./input.txt', 'utf8').split(' ').map(Number)
 
 function solution (n, k) {
   const result = []
-  console.log(n, k)
+
+  let acc = 0;
+
+  // 빠진 수를 생각 안하니 정답이 이상하게나옴. linked list 로 푸는게 가장 깔끔할거같은데...
+  while (n > result.length) {
+    const value = n > k + acc ? k + acc : (k + acc) - n
+
+      result.push(value)
+      acc = value
+  }
+  console.log(result)
 }
 
 solution(n, k)
