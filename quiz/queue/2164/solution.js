69a16ec89f8da96fb9d68b561af3604a86e34a4d
jongkeun-kim <kjkandrea@gmail.com>
2022-01-31 14:03:27 +0900

    Add 2164 my queue

--- quiz/queue/2164/solution.js
+++ quiz/queue/2164/solution.js
@@ -3,14 +3,33 @@ import fs from "fs"
 const range = Number(fs.readFileSync('./input.txt', 'utf8'))
 
 class Node {
-  constructor () {
-
+  constructor (value) {
+    this.value = value
+    this.next = null
   }
 }
 
 class Queue {
   constructor () {
+    this.head = null
+    this.tail = null
+  }
 
+  enqueue (value) {
+    const node = new Node(value)
+    if (this.head === null) {
+      this.head = node
+      this.tail = node
+    } else {
+      this.tail.next = node
+      this.tail = node
+    }
+  }
+
+  dequeue () {
+    const value = this.head.value
+    this.head = this.head.next
+    return value
   }
 }
 
