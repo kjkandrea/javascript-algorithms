d7acf0533deb76ac42faeb1b8ba6291e9aca6481
jongkeun-kim <kjkandrea@gmail.com>
2022-02-02 13:52:52 +0900

    Add 2750 mergeSort structure

--- quiz/sort/2750/solution.js
+++ quiz/sort/2750/solution.js
@@ -3,4 +3,20 @@ import fs from "fs"
 const lines = fs.readFileSync('./input.txt', 'utf8').split('\n')
 lines.shift()
 const numbers = lines.map(Number)
-console.log(numbers)
+
+function mergeSort(numbers) {
+  if (numbers.length === 1) return numbers;
+
+  const mid = Math.floor(numbers.length/2);
+  const left = numbers.slice(0, mid)
+  const right = numbers.slice(mid)
+
+  console.log('left :', left)
+  console.log('right :', right)
+}
+
+function solution(numbers) {
+  console.log(mergeSort(numbers))
+}
+
+solution(numbers)
