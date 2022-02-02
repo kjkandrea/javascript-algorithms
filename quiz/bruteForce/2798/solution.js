e6cd11a26d32d8a5d039f28c9b650feb9f8e4d68
jongkeun-kim <kjkandrea@gmail.com>
2022-02-02 14:56:18 +0900

    Add 2798 answer : fail

--- quiz/bruteForce/2798/solution.js
+++ quiz/bruteForce/2798/solution.js
@@ -5,7 +5,21 @@ const n = Number(lines[0].split(' ')[1])
 const cards = lines[1].split(' ').map(Number)
 
 function solution(n, cards) {
-  console.log(n, cards)
+  let near = 0;
+
+  for (let i = 0; i < cards.length; i += 1) {
+    for (let j = i + 1; j < cards.length; j += 1) {
+      for (let k = j + 1; k < cards.length; k += 1) {
+        const sum = cards[i] + cards[j] + cards[k];
+        if (sum === n) return n
+        if (Math.abs(near - n) > Math.abs(sum - n)) near = sum
+      }
+    }
+  }
+
+  return near;
 }
 
+console.log(
 solution(n, cards)
+)
