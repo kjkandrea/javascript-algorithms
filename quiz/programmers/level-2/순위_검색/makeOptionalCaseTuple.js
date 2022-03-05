f4038f418a53ad1165ef1643f502a4d140410b9e
jongkeun-kim <kjkandrea@gmail.com>
2022-03-05 19:11:31 +0900

    Add makeOptionalCaseTuple
    문제풀때 쓸거임

--- quiz/programmers/level-2/순위_검색/makeOptionalCaseTuple.js
+++ quiz/programmers/level-2/순위_검색/makeOptionalCaseTuple.js
@@ -1,5 +1,17 @@
 function makeOptionalCaseTuple (array) {
-  const emptyCaseArray = array.concat(null)
+  const result = []
+
+  array.forEach((value, index, origin) => {
+    const rest = origin.slice(index)
+
+    result.push(
+      Array(origin.length - rest.length)
+        .fill(null)
+        .concat(rest)
+    )
+  })
+
+  return result;
 
   // 조합 알고리즘 필요
 }
@@ -8,5 +20,4 @@ const data = ['새빨간', '멋쟁이', '토마토']
 
 console.log(
   makeOptionalCaseTuple(data),
-  data
 )
