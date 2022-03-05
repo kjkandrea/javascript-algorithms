7c565cec1c630dff707bcb4cd6ba1562ff6598ae
jongkeun-kim <kjkandrea@gmail.com>
2022-03-05 18:56:07 +0900

    Add new subject : p & c

---
+++ quiz/programmers/level-2/순위_검색/makeOptionalCaseTuple.js
@@ -0,0 +1,12 @@
+function makeOptionalCaseTuple (array) {
+  const emptyCaseArray = array.concat(null)
+
+  // 조합 알고리즘 필요
+}
+
+const data = ['새빨간', '멋쟁이', '토마토']
+
+console.log(
+  makeOptionalCaseTuple(data),
+  data
+)
--- quiz/programmers/level-2/순위_검색/solution.js
+++ quiz/programmers/level-2/순위_검색/solution.js
@@ -1,103 +1,24 @@
 import input from './input.js'
 
-// 성능개선 하면서 버그 생긴듯. 테케 다 통과 못함. 망함
-// * 해시테이블로 만들면서 데이터 잘못맵핑했나
-// * 검색 조건이 잘못되었나?
-class UserTable {
-  constructor (payload, query) {
-    this.setTable(payload)
-    this.queries = UserTable.parseQueries(query)
-  }
+const parseItem = item => {
+  return item.split(' ')
+}
 
-  allSearch = () => this.queries.map(this.search)
+const getInfoCases = info => {
+  const result = info.map(data => makeCase(parseItem(data)))
 
-  search = query => {
-    const { lang, position, level, flavor, score } = query
-
-    const index = UserTable.binarySearchIndex(this.scores, score,
-      (current, score) => current >= score)
-    const matchedScore = this.scores.slice(index)
-
-    return matchedScore.map(
-      score => this.table.get(score),
-    ).flat().filter(data => {
-        console.log('data : ', data.lang, data.position, data.level, data.flavor, data.score)
-        console.log('query : ',lang, position, level, flavor, score)
-        // (score === '-' || Number(data.score) >= Number(score))
-        return (lang === '-' || data.lang === lang)
-        && (position === '-' || data.position === position)
-        && (level === '-' || data.level === level)
-        && (flavor === '-' || data.flavor === flavor)
-      }
-    ).length
-  }
-
-  setTable (payload) {
-    const scores = new Set()
-    const table = new Map()
-
-    payload.forEach(data => {
-      const [lang, position, level, flavor, stringScore] = data.split(' ')
-
-      const score = Number(stringScore)
-
-      // WARN : SIDE EFFECT
-      scores.add(score)
-
-      const prev = table.get(score)
-      if (prev) {
-        table.set(score, prev.concat({ lang, position, level, flavor, score }))
-      }
-      else {
-        table.set(score, [{ lang, position, level, flavor, score }])
-      }
-    })
-
-    this.scores = [...scores].sort((a, b) => a - b)
-    this.table = table
-  }
-
-  static parseQueries (query) {
-    return query.map(data => {
-      const [lang, position, level, favorAndScore] = data.split(' and ')
-      const [flavor, stringScore] = favorAndScore.split(' ')
-      return { lang, position, level, flavor, score: Number(stringScore) }
-    })
-  }
-
-  static binarySearchIndex (sortedArray, findValue, predict) {
-    let left = 0
-    let right = sortedArray.length - 1
-    let mid = Math.floor((left + right) / 2)
-
-    while (left < right) {
-      // 여기가 문제?????
-      if (predict(sortedArray[mid], findValue)) {
-        return mid
-      }
-
-      if (sortedArray[mid] < findValue) {
-        left = mid + 1
-      }
-      else {
-        right = mid - 1
-      }
-
-      mid = Math.floor((left + right) / 2)
-    }
-
-    return 0
-  }
+  return result;
 }
 
 const solution = (info, query) => {
-  return new UserTable(info, query).allSearch()
+  console.log(getInfoCases(info))
+  console.log(query[0])
 }
 
 function main () {
   const [payload, queries, o] = input
   const result = solution(payload, queries)
-  console.log(result, o, result.join() === o.join())
+  // console.log(result, o, result.join() === o.join())
 }
 
 main()
