649b76274a6c526c807f577e018b9ec3da9621cb
jongkeun-kim <kjkandrea@gmail.com>
2022-03-05 19:46:39 +0900

    Add UserTable

--- quiz/programmers/level-2/순위_검색/solution.js
+++ quiz/programmers/level-2/순위_검색/solution.js
@@ -1,31 +1,44 @@
 import input from './input.js'
 
-const parseItem = item => {
-  return item.split(' ')
-}
+class UserTable {
+  constructor (rawInfo) {
+    this.data = UserTable.parseInfo(rawInfo)
+  }
 
-const getArrayOptionalCases = (array, emptyValue)  => {
-  const result = []
+  static parseInfo (info) {
+    const result = new Map()
+    info.forEach(data => {
+      const array = data.split(' ')
+      const score = Number(array.pop())
+      result.set(score, UserTable.makeArrayOptionalCases(array))
+    })
+    return result;
+  }
 
-  array.forEach((value, index, origin) => {
-    const rest = origin.slice(index)
-    const emptyArray = Array(origin.length - rest.length).fill(emptyValue)
-    result.push(emptyArray.concat(rest))
-  })
+  static makeArrayOptionalCases = (array, emptyValue = '-') => {
+    const result = []
 
-  return result;
+    array.forEach((value, index, origin) => {
+      const rest = origin.slice(index)
+      const emptyArray = Array(origin.length - rest.length).fill(emptyValue)
+      result.push(emptyArray.concat(rest).join(' and '))
+    })
+
+    result.push(Array(array.length).fill(emptyValue).join(' and '))
+    return result;
+  }
+
 }
 
 const solution = (info, query) => {
-  const testArray = info[0].split(' ');
-  testArray.pop()
-  console.log(getArrayOptionalCases(testArray, '-'))
-  // console.log(query[0])
+  const { data } = new UserTable(info)
+  console.log(data)
 }
 
 function main () {
   const [payload, queries, o] = input
   const result = solution(payload, queries)
+  console.log(queries)
   // console.log(result, o, result.join() === o.join())
 }
 
