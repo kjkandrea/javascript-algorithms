98f88cf21bd806317cdfc68ae24d0c94901e80e7
jongkeun-kim <kjkandrea@gmail.com>
2022-03-12 18:18:00 +0900

    Add heap push

--- 07_Heap/heap.js
+++ 07_Heap/heap.js
@@ -6,11 +6,17 @@ class Heap {
   push(value) {
     this.values.push(value)
 
-    const curIdx = this.values.length - 1
-    const parIdx = Math.floor(curIdx / 2)
+    let curIdx = this.values.length - 1
+    let parIdx = Math.floor(curIdx / 2)
 
-    console.log('curIdx : ', curIdx)
-    console.log('parIdx : ', parIdx)
+    while (parIdx !== 0  && this.values[parIdx] < value) {
+      const temp = this.values[parIdx]
+      this.values[parIdx] = value
+      this.values[curIdx] = temp
+
+      curIdx = parIdx
+      parIdx = Math.floor(curIdx / 2)
+    }
   }
 }
 
@@ -19,6 +25,7 @@ function main() {
 
   heap.push(45)
   heap.push(44)
+  heap.push(46)
 
   console.log(heap.values)
 }
