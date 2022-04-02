0f9a13909c20a3dfa56f172e3bc60d0a18c148fa
jongkeun-kim <kjkandrea@gmail.com>
2022-04-02 15:22:52 +0900

    Add ToSubstring

--- 11_Permutations_and_Combinations/toSubstring.js
+++ 11_Permutations_and_Combinations/toSubstring.js
@@ -2,25 +2,40 @@ const str = 'banana'
 
 const range = num => Array.from(Array(num), (_, i) => i + 1)
 
+class ToSubstring {
+  constructor (str) {
+    this.str = str
+    this.range = range(str.length).reverse()
+  }
+
+  // O(n^2)
+  getAll () {
+    const result = []
+    const { length } = this.str
+
+    const ran = range(length).reverse()
+    let headIndex = 0
+    ran.forEach(v => {
+      while (this.str[v + headIndex - 1]) {
+        const tailIndex = v + headIndex
+        const string = this.str.substring(headIndex, tailIndex)
+        result.push(string)
+        headIndex += 1
+      }
+      headIndex = 0
+    })
+
+    return result
+  }
+}
+
 function toSubstring (str) {
-  const result = []
-  const { length } = str
+  const toSub = new ToSubstring(str)
 
-  const ran = range(length).reverse()
-  let headIndex = 0;
-  ran.forEach(v => {
-    while (str[v + headIndex - 1]) {
-      const tailIndex = v + headIndex;
-      const string = str.substring(headIndex, tailIndex)
-      result.push(string)
-      headIndex += 1;
-    }
-    headIndex = 0;
-  })
-
-  return result;
+  return toSub.getAll()
 }
 
 console.log(
-  toSubstring(str) // 'banana' | 'banan' | 'anana' | 'bana' | 'anan' | 'nana' | 'ban' | 'ana' ...
+  toSubstring(str), // 'banana' | 'banan' | 'anana' | 'bana' | 'anan' | 'nana' |
+                   // 'ban' | 'ana' ...
 )
